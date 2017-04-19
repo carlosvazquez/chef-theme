75f3d8864e9b6524171887a124caec2518067c85
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2017-04-19 13:35:24 -0500

    fix webpack server config to lightweight

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,21 +1,13 @@
 'use strict';
-
 var webpack = require('webpack');
 var path = require('path');
-var WebpackShellPlugin = require('webpack-shell-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpackUglifyJsPlugin = require('webpack-uglify-js-plugin');
 
 module.exports = {
   entry: {
-    app: './src/app.js'
+    app: './src/prod.js'
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'assets/[name].js',
-    publicPath: '/assets/'
+    path: path.resolve(__dirname, 'public')
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
@@ -24,54 +16,6 @@ module.exports = {
     inline: false,
     port: 9000,
     stats: 'errors-only',
-    filename: "app.js"
-  },
-  module: {
-    rules: [
-    {
-      test: /\.(js)$/,
-      loader: 'babel-loader',
-      exclude: /node_module/,
-      query: {
-        presets: ['es2015']
-      }
-    },
-    {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('css-loader!sass-loader')
-    },
-    {
-      test: /\.less$/,
-      loader: ExtractTextPlugin.extract('css-loader!less-loader')
-    },
-    {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('css-loader')
-    },
-    {
-      test: /\.exec.js$/,
-      use: [ 'script-loader' ]
-    }
-    ]
-  },
-  externals: {
-    jquery: 'jQuery',
-    slick: 'slick-carousel'
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-    new ExtractTextPlugin('assets/styles.css'),
-    new CopyWebpackPlugin([
-      { from: './src/html/home.twig', to: 'home.html' }
-      ], { copyUnmodified: false }),
-    new WebpackShellPlugin({
-      onBuildStart: ['node convert-twig-params.js'],
-      onBuildEnd: ['node convert-twig-params.js'],
-      onBuildExit: ['node convert-twig-params.js']
-    })
-  ]
+    filename: "prod.js"
+  }
 }
